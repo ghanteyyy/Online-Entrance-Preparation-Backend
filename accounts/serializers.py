22a2e5a476f38d57a94a27c1f2cea46f5180c9da
ghanteyyy <ghanteyyy@gmail.com>
2026-03-07 16:42:09 +0545

    Fixed serializer field mapping for date_of_birth

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,6 +4,7 @@ from .models import CustomUser
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    date_of_birth = serializers.DateField(source='dob')
 
     class Meta:
         model = CustomUser
@@ -20,6 +21,8 @@ class RegisterSerializer(serializers.ModelSerializer):
 
 
 class MeSerializer(serializers.ModelSerializer):
+    date_of_birth = serializers.DateField(source='dob')
+
     class Meta:
         model = CustomUser
-        fields = ["id", "name", "email", "gender", "date_of_birth", "profile_image", "is_staff", "is_superuser", "is_active"]
+        fields = ["id", "name", "email", "gender", "date_of_birth", "profile_image"]
