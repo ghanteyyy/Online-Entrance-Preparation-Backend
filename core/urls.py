030b0949d7d80ee8713f05e95e2d11090a64f49d
ghanteyyy <ghanteyyy@gmail.com>
2026-03-30 20:28:09 +0545

    Added url endpoints for Exams, Programmes and debug_toolbar

--- core/urls.py
+++ core/urls.py
@@ -25,7 +25,10 @@ from drf_spectacular.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
+    path('', include('Programmes.urls')),
+    path('', include('Exams.urls')),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
