35f4b607d72c29a7b712a31c5bf2d79124297d69
ghanteyyy <ghanteyyy@gmail.com>
2026-04-04 18:11:21 +0545

    Made production ready for deployment

--- accounts/cookies.py
+++ accounts/cookies.py
@@ -3,21 +3,21 @@ from django.conf import settings
 REFRESH_COOKIE_NAME = "refresh_token"
 
 
-def set_refresh_cookie(response, refresh_token: str):
+def set_refresh_cookie(response, refresh_token: str) -> None:
     response.set_cookie(
-        REFRESH_COOKIE_NAME,
-        refresh_token,
+        key=REFRESH_COOKIE_NAME,
+        value=refresh_token,
         httponly=True,
-        secure=False,
-        samesite="Lax",   # cross-site
-        path="/api/auth/", # restrict where cookie is sent
+        secure=not settings.DEBUG,
+        samesite="None" if not settings.DEBUG else "Lax",
+        path="/api/auth/",
         max_age=int(settings.SIMPLE_JWT["REFRESH_TOKEN_LIFETIME"].total_seconds()),
     )
 
 
-def clear_refresh_cookie(response):
+def clear_refresh_cookie(response) -> None:
     response.delete_cookie(
-        REFRESH_COOKIE_NAME,
+        key=REFRESH_COOKIE_NAME,
         path="/api/auth/",
-        samesite="Lax",
+        samesite="None" if not settings.DEBUG else "Lax",
     )
