6a05f43d5e155884c6c5746047d2a3b37acbceef
ghanteyyy <ghanteyyy@gmail.com>
2026-04-04 17:06:24 +0545

    Configured http cookie for development

--- accounts/cookies.py
+++ accounts/cookies.py
@@ -8,8 +8,8 @@ def set_refresh_cookie(response, refresh_token: str):
         REFRESH_COOKIE_NAME,
         refresh_token,
         httponly=True,
-        secure=True,
-        samesite="None",   # cross-site
+        secure=False,
+        samesite="Lax",   # cross-site
         path="/api/auth/", # restrict where cookie is sent
         max_age=int(settings.SIMPLE_JWT["REFRESH_TOKEN_LIFETIME"].total_seconds()),
     )
@@ -19,5 +19,5 @@ def clear_refresh_cookie(response):
     response.delete_cookie(
         REFRESH_COOKIE_NAME,
         path="/api/auth/",
-        samesite="None",
+        samesite="Lax",
     )
